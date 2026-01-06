e28c3f7bd321c9a17702d9c5a27931e6049beb02
AnderT87 <anderayov@gmail.com>
2026-01-06 17:34:10 -0500

    Complejidad Algoritmica 1

---
+++ src/main/java/ec/edu/espoch/complejidadalgoritmica/ComplejidadAlgoritmica.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package ec.edu.espoch.complejidadalgoritmica;
+
+/**
+ *
+ * @author Admin
+ */
+public class ComplejidadAlgoritmica {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        
+        // Ahora podemos llamar al método porque ambos son static
+        System.out.println("El mayor es: " + Metodo1(a, b));
+        System.out.println("---------------------------");
+        System.out.println("El mayor es: " + Metodo2(a, b));
+    }
+
+    // Agregamos 'static' y parámetros (int n1, int n2)
+    public static int Metodo1(int n1, int n2) {
+        int resultado = 0;
+        long inicio = System.nanoTime();
+        
+        for (int i = 0; i < 40; i++) {
+            resultado = (n1 > n2) ? n1 : n2;
+        }
+        
+        long fin = System.nanoTime();
+        System.out.println("Tiempo logico: " + (fin - inicio) + " ns");
+        return resultado; 
+    }
+
+    // Agregamos 'static' y parámetros (int n1, int n2)
+    public static int Metodo2(int n1, int n2) {
+        int resultado = 0;
+        long inicio = System.nanoTime();
+        
+        for (int i = 0; i < 40; i++) {
+            if (n1 > n2) {
+                resultado = n1;
+            } else {
+                resultado = n2;
+            }
+        }
+        
+        long fin = System.nanoTime();
+        System.out.println("Tiempo if-else: " + (fin - inicio) + " ns");
+        return resultado; 
+    }
+}
